6081127619a71d86ab9da89234cc7fcc3be2c862
Easter-12 <easterinternational90@gmail.com>
2025-07-19 03:17:31 +0000

    FIX: Remove vitePreprocess to fix build

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,8 @@
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-
 	kit: {
-		// THIS IS THE CRITICAL LINE THAT MUST BE CORRECT
 		adapter: adapter()
 	}
 };
